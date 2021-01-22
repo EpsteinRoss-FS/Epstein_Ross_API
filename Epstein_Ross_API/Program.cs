acde395b8dfb4256fba89b01644867bb5cb99d4c
EpsteinRoss-FS <repstein@student.fullsail.edu>
2021-01-22 15:03:22 -0500

    initial commit - added classes

---
+++ Epstein_Ross_API/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Epstein_Ross_API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
